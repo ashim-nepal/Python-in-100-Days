ca13b2ec74be71c5ab7c75c7c74d607dfa088138
ashim-nepal <npashim015@gmail.com>
2024-08-06 07:49:48 +0545

    day 28, 29, 30

---
+++ Python-in-100-days-ReplitCourse/day30_100days.py
@@ -0,0 +1,14 @@
+"""
+Day 30 Challenge: f for format print
+Simple day log from day 1 - day 30!
+"""
+
+print("30 Days Down - What did you think?")
+print()
+for i in range(1, 31):
+  thought = input(f"Day {i}:\n")
+  print()
+  myText = f"You thought Day {i} was"
+  print(f"{myText:^35}")
+  print(f"{thought:^35}")
+  print()
