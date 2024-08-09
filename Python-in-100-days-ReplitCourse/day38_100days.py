285b64349469ca49fcb687bcd3e5ad30c725c975
ashim-nepal <npashim015@gmail.com>
2024-08-09 14:55:29 +0545

    day37, 38, 39

---
+++ Python-in-100-days-ReplitCourse/day38_100days.py
@@ -0,0 +1,25 @@
+"""Day 38 Challenge: Colorful print
+Making colorful print using string and function on rainbow colors!
+"""
+
+
+def colorChange(color):
+  if color == "r":
+    print("\033[31m", end="")
+  elif color == " ":
+    print("\033[0m", end="")
+  elif color == "b":
+    print("\033[34m", end="")
+  elif color == "y":
+    print("\033[33m", end="")
+  elif color == "g":
+    print("\033[32m", end="")
+  elif color == "p":
+    print("\033[35m", end="")
+
+
+sentence = input("What sentence do you want as rainbow: ")
+for letter in sentence:
+  colorChange(letter.lower())
+  print(letter, end="")
+print()
