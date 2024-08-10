4ca8cbd2d23ade0421bb4ccb5f590bd617c680dd
ashim-nepal <npashim015@gmail.com>
2024-08-10 09:27:57 +0545

    day 40, 41, 42

---
+++ Python-in-100-days-ReplitCourse/day41_100days.py
@@ -0,0 +1,13 @@
+"""
+Day 41 Challenge: Keys and Values dynamic use in Dictionary
+Simple website storing with user input in dictionary
+"""
+
+website = {"name": None, "url": None, "desc": None, "rating": None}
+
+for name in website.keys():
+  website[name] = input(f"{name}: ")
+
+print()
+for name, value in website.items():
+  print(f"{name}: {value}")
