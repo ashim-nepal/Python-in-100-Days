0240472e66178c5369eed64221078fac0eab08df
ashim-nepal <npashim015@gmail.com>
2024-08-11 12:51:54 +0545

    day 43, 44, 45

---
+++ Python-in-100-days-ReplitCourse/day43_100days.py
@@ -0,0 +1,30 @@
+"""
+Day 43 Challenge: Multi /dimensional list in python
+Simple 2d list
+"""
+
+
+import random
+
+bingo = []
+
+def ran():
+  number = random.randint(1,90)
+  return number
+
+def prettyPrint():
+  for row in bingo:
+    print(row)
+
+numbers = []
+for i in range(8):
+  numbers.append(ran())
+
+numbers.sort()
+
+bingo = [ [ numbers[0], numbers[1], numbers[2]],
+          [ numbers[3], "BINGO", numbers[4] ],
+          [ numbers [5], numbers[6], numbers[7]]
+        ]
+
+prettyPrint()
